9c4366a6e7d5e13d678e70226de0198ac01668f4
Dyleef <145235710+Dyleef@users.noreply.github.com>
2026-02-11 02:52:40 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "msd-v10";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then((c) => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match("./index.html"))
+  );
+});
